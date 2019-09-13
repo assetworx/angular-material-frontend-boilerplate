c31016ceffd47b5bc7c64dd40d9d1c48671bd00f
Christian Versloot <mail@christianversloot.nl>
2019-09-13 16:40:19 +0200

    Fix issue with default home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  title = 'GSWRX Angular Material Bootstrap';
+
   constructor() { }
 
   ngOnInit() {
